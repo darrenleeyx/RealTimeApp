c52e340edbd38aeb1304f48bd40f114d867b97e7
Darren <59044608+darrenleeyx@users.noreply.github.com>
2024-12-10 22:09:52 +0800

    Setup signalr in console

--- RealTimeApp.Console/Program.cs
+++ RealTimeApp.Console/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+var connection = new HubConnectionBuilder()
+               .WithUrl("https://localhost:7166/notifications")
+               .Build();
+
+connection.On<string>("ReceiveNotification", (message) =>
+{
+    Console.WriteLine($"Notification received: {message}");
+});
+
+try
+{
+    await connection.StartAsync();
+    Console.WriteLine("Connection started.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error starting connection: {ex.Message}");
+}
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
+await connection.StopAsync();
