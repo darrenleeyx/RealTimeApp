086a020aab1d458761157e32a6a29d319e55192e
Darren <59044608+darrenleeyx@users.noreply.github.com>
2024-12-10 22:04:38 +0800

    Setup signalr in Api

---
+++ RealTimeApp.Api/NotificationsHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RealTimeApp.Api;
+
+public class NotificationsHub : Hub<INotificationClient>
+{
+    public override async Task OnConnectedAsync()
+    {
+        await Clients.Caller.ReceiveNotification("Welcome to the notifications hub!");
+
+        await base.OnConnectedAsync();
+    }
+
+    public async Task SendNotification(string message)
+    {
+        await Clients.All.ReceiveNotification(message);
+    }
+}
+
+public interface INotificationClient
+{
+    Task ReceiveNotification(string message);
+}
---
+++ RealTimeApp.Api/ServerTimeNotifier.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RealTimeApp.Api;
+
+public class ServerTimeNotifier : BackgroundService
+{
+    private readonly IHubContext<NotificationsHub, INotificationClient> _hubContext;
+    public ServerTimeNotifier(IHubContext<NotificationsHub, INotificationClient> hubContext)
+    {
+        _hubContext = hubContext;
+    }
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await _hubContext.Clients.All.ReceiveNotification($"Server time: {DateTimeOffset.Now}");
+            await Task.Delay(5000, stoppingToken);
+        }
+    }
+}
